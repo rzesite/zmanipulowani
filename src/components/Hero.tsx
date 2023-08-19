811591a28b9f679b48474a8e140ea1219ef18773
Rzepaw <rzepaw@gmail.com>
2023-08-19 10:58:12 +0200

    Firebase configured

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { Button } from '@/components/Button'
 import { GridPattern } from '@/components/GridPattern'
 import { StarRating } from '@/components/StarRating'
-import coverImage from '@/images/cover.png'
+import coverImage from '@/images/cover.jpg'
 
 function Testimonial() {
   return (
@@ -13,15 +13,14 @@ function Testimonial() {
       </div>
       <blockquote className="mt-2">
         <p className="font-display text-xl font-medium text-slate-900">
-          “This method of designing icons is genius. I wish I had known this
-          method a lot sooner.”
+          “Jest duża wydawnicza bomba, bo zawsze gdy polityk pisze książkę jesteśmy podekscytowani.”
         </p>
       </blockquote>
       <figcaption className="mt-2 text-sm text-slate-500">
         <strong className="font-semibold text-blue-600 before:content-['—_']">
-          Stacey Solomon
+          Jan Kowalski
         </strong>
-        , Founder at Retail Park
+        , Polskie Radio
       </figcaption>
     </figure>
   )
@@ -50,18 +49,14 @@ export function Hero() {
         <div className="bg-white pt-16 lg:col-span-7 lg:bg-transparent lg:pl-16 lg:pt-0 xl:pl-20">
           <div className="mx-auto px-4 sm:px-6 md:max-w-2xl md:px-4 lg:px-0">
             <h1 className="font-display text-5xl font-extrabold text-slate-900 sm:text-6xl">
-              Get lost in the world of icon design.
+              Zmanipulowani
             </h1>
             <p className="mt-4 text-3xl text-slate-600">
-              A book and video course that teaches you how to design your own
-              icons from scratch.
+              czyli jak dobrzy ludzie dają się uwieść inteligentnej propagandzie
             </p>
             <div className="mt-8 flex gap-4">
-              <Button href="#free-chapters" color="blue">
-                Get sample chapter
-              </Button>
               <Button href="#pricing" variant="outline" color="blue">
-                Buy book
+                Kup
               </Button>
             </div>
           </div>

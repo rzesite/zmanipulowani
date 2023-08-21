fd33873807ddbcf35ff13e7d6009fc82f7f8d19c
Rzepaw <rzepaw@gmail.com>
2023-08-21 12:06:36 +0200

    z index

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,16 +5,16 @@ import witLogo from '@/images/wit.png'
 export function Footer() {
   return (
     <footer className="relative pb-20 pt-5 sm:pb-32 sm:pt-14">
-      <div className="absolute inset-x-0 top-0 h-32 text-slate-900/10 [mask-image:linear-gradient(white,transparent)]">
+      <div className="absolute inset-x-0 top-0 h-32 text-slate-900/10 [mask-image:linear-gradient(white,transparent)] z-50">
         <GridPattern x="50%" />
       </div>
-        <div className='flex items-center justify-center'>
-          <Image
-            className="inset-0 h-44 w-44 object-cover grayscale"
-            src={witLogo}
-            alt="Wolność i Tradycja"
-          />
-        </div>
+      <div className='flex items-center justify-center'>
+        <Image
+          className="inset-0 h-44 w-44 object-cover grayscale"
+          src={witLogo}
+          alt="Wolność i Tradycja"
+        />
+      </div>
     </footer>
   )
 }
